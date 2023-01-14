125c113ee9c5a8fbb94def9ce3d71858f3091836
Albert Alvarez Estelles <susokukapu@gmail.com>
2023-01-14 12:44:35 +0100

    delete separated js

--- bases/funciones/args-rest.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-(() => {
-    const fullName = (firstName, ...restOfName) => {
-        return `${firstName} ${restOfName.join(' ')}`;
-    };
-    const superman = fullName('Clark', 'Joseph', 'Kent', 'Superman', 'Kal-El');
-    console.log({ superman });
-})();
--- bases/tipos/boolean.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-// PONEMOS funciones autoinvocadas para qur no haya problemas con el scope de las variables
-(() => {
-    let isSuperman = true;
-    let isBatman = false;
-    isSuperman = (isBatman) ? true : false;
-    console.log({ isSuperman });
-})();
